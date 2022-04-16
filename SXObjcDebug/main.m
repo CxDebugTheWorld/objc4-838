24d0a8bdf69a859d993a8e018a251cf853b1b73a
cuixing <632375960@qq.com>
2022-04-16 16:27:41 +0800

    objc-838 init

---
+++ SXObjcDebug/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SXObjcDebug
+//
+//  Created by Allin on 2022/4/13.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+#import <malloc/malloc.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello World!");
+    }
+    return 0;
+}
